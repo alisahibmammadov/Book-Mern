90ae10fa7aaea51a68c6d74eedd12478615a49d7
alisahib <alisahibmammadov98@gmail.com>
2023-08-08 19:51:31 +0400

    Update

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
 // require("dotenv").config()
-if(process.env.NODE_ENV  !== 'production'){
-    require("dotenv").configDotenv()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 const expressLayouts = require("express-ejs-layouts");
-const indexRouter = require("./routes/index");
+const bodyParser = require("body-parser");
+const app = express();
 
+const indexRouter = require("./routes/index.js");
+const authorRouter = require("./routes/authors.js");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
-const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
 });
-const db = mongoose.connection
-db.on('error',error=>console.log(error))
-db.once('open',()=>console.log('Connected to MongoDB'))
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Connected to MongoDB"));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => console.log("Server is running"));
